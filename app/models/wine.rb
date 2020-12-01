4174577eb585db5a4d9aa485d2ee5d6cbeb7105f
labradorescence <orangenewyorkcreativeagency@gmail.com>
2020-12-01 15:40:22 -0500

    update

--- app/models/wine.rb
+++
@@ -1,6 +0,0 @@
-class Wine < ApplicationRecord
-
-    has_many :reviews
-    validates_presence_of :name
-    
-end
--- app/serializers/wine_serializer.rb
+++
@@ -1,5 +0,0 @@
-class WineSerializer < ActiveModel::Serializer
-  has_many :reviews
-  attributes :id, :name, :producer, :ingredient, :wine_type, :region, :country, :price, :rating, :note, :image
-end
-
